20f849d6e8714cb5330e8af52ca16e24cde62909
Flavio Martins <flaviomartins@acm.org>
2017-05-09 13:59:39 +0100

    add extract_tweetids.pl

---
+++ twitter-tools-core/src/main/perl/extract_tweetids.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+# Scans a directory containing the output of the stream crawler and
+# extracts the tweetids
+
+$directory = shift or die "$0 [directory]";
+
+for $f ( `ls $directory` ) {
+    chomp($f);
+    my $path = "$directory/$f";
+
+    open(DATA, "gunzip -c $path | grep '{\"created_at\"' | ");
+    while ( my $line = <DATA> ) {
+	if ( $line =~ m/{"created_at":.*,"id":(\d+),/ ) {
+	    print "$1\n";
+	}
+    }
+    close(DATA);
+}
